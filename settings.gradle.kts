a41bfb1dc729782323f2bf215bc2da1f45dafea1
Noflus101 <pootisspy931@gmail.com>
2024-11-05 18:44:17 +0700

    Initial commit by Eric Yoel Pls

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ListView_EricYoel_C14220036"
+include(":app")
+ 
